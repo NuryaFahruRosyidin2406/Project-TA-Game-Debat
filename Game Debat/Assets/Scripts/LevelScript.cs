6a45fb98d5d783bdcc4bd35e1774c44eaf0010ad
Hanif-2610 <60161283+Hanif-2610@users.noreply.github.com>
2022-06-22 12:02:18 +0800

    Add Level Unlock
    Now the level will be unlock only if the previous level were completed

---
+++ Game Debat/Assets/Scripts/LevelScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelScript : MonoBehaviour
+{
+    public void Pass()
+    {
+        int currentLevel = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentLevel >= PlayerPrefs.GetInt("levelsUnlocked"))
+        {
+            PlayerPrefs.SetInt("levelsUnlocked", currentLevel + 1);
+        }
+
+        Debug.Log("LEVEL" + PlayerPrefs.GetInt("levelsUnlocked") + " UNLOCKED");
+    }
+}
